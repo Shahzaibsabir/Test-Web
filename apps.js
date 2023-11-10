12b46eddb8b0d33b965c863991de012c53c3e85c
SHAHZAIB KHAN <56146605+Shahzaibsabir@users.noreply.github.com>
2023-11-10 04:37:56 -0800

    Update apps.js
    Js Function Create

--- apps.js
+++ apps.js
@@ -15,3 +15,23 @@ input_email = "Please enter your email address"
 
 // alert("Please enter your email address)
 alert("Hello! I am an alert box!!");
+
+
+
+
+Var input_email
+
+input_email = "Please enter your email address"
+
+// alert("Please enter your email address)
+alert("Hello! I am an alert box!!");
+
+
+
+
+Var input_email
+
+input_email = "Please enter your email address"
+
+// alert("Please enter your email address)
+alert("Hello! I am an alert box!!");
