927cec079d884dd5dfbd0ee98ad91a97c9dc69f8
SHAHZAIB KHAN <56146605+Shahzaibsabir@users.noreply.github.com>
2023-11-10 04:41:39 -0800

    Update apps.js
    game add

--- apps.js
+++ apps.js
@@ -73,3 +73,21 @@ input_email = "Please enter your email address"
 
 // alert("Please enter your email address)
 alert("Hello! I am an alert box!!");
+
+
+Var input_email
+
+input_email = "Please enter your email address"
+
+// alert("Please enter your email address)
+alert("Hello! I am an alert box!!");
+
+
+
+
+Var input_email
+
+input_email = "Please enter your email address"
+
+// alert("Please enter your email address)
+alert("Hello! I am an alert box!!");
