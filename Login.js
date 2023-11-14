c8ddc5a91366ecd212894dba1804dbdeab3e12ef
SHAHZAIB KHAN <56146605+Shahzaibsabir@users.noreply.github.com>
2023-11-14 10:12:15 -0800

    Update Login.js
    Check User or Password

--- Login.js
+++ Login.js
@@ -1,1 +1,16 @@
+function login()
+	{
+		var uname = document.getElementById("email").value;
+		var pwd = document.getElementById("pwd1").value;
+		var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+		if(uname =='')
+		{
+			alert("please enter user name.");
+		}
+		else if(pwd=='')
+		{
+        	alert("enter the password");
+		}
+
+	
 
