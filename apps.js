f01b4fb915856d68e8d53b821e0c997523575e73
SHAHZAIB KHAN <56146605+Shahzaibsabir@users.noreply.github.com>
2023-11-16 10:02:03 -0800

    Update apps.js
    email vefication

--- apps.js
+++ apps.js
@@ -1,3 +1,21 @@
+Var input_email
+
+input_email = "Please enter your email address"
+
+// alert("Please enter your email address)
+alert("Hello! I am an alert box!!");
+
+
+
+
+Var input_email
+
+input_email = "Please enter your email address"
+
+// alert("Please enter your email address)
+alert("Hello! I am an alert box!!")
+
+
 
 Var input_email
 
