260baf1fd87ef47d07538d0a18701f4d39ffeb7f
SHAHZAIB KHAN <56146605+Shahzaibsabir@users.noreply.github.com>
2023-11-27 10:04:56 -0800

    Update Login.js
     commdnts login code

--- Login.js
+++ Login.js
@@ -26,6 +26,22 @@ function login()
 		{
         	alert("enter the password");
 		}
+
+
+
+		function login()
+	{
+		var uname = document.getElementById("email").value;
+		var pwd = document.getElementById("pwd1").value;
+		var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+		if(uname =='')
+		{
+			alert("please enter user name.");
+		}
+		else if(pwd=='')
+		{
+        	alert("enter the password");
+		}
 --------------------------------------------
 {
 			alert("Enter valid email id.");
